5a069be826927250c68bd9aac25c8e1319d36cce
Vashika S K <150437929+Vashika17@users.noreply.github.com>
2026-06-23 22:40:27 +0530

    Update train.py

--- Churn Prediction/train.py
+++ Churn Prediction/train.py
@@ -2,17 +2,20 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pickle
+from preprocess import load_data, preprocess
+from evaluate import evaluate
 
-data = pd.read_csv("churn.csv")
+data = load_data("data/churn.csv")
+data = preprocess(data)
 
 X = data.drop("churn", axis=1)
 y = data["churn"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-pickle.dump(model, open("model.pkl", "wb"))
+evaluate(model, X_test, y_test)
 
-print("Model trained")
+pickle.dump(model, open("models/model.pkl", "wb"))
