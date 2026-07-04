e75e7d79a97ea8b5a6d33d7f64ab5d76971b6430
Vashika S K <150437929+Vashika17@users.noreply.github.com>
2026-07-04 11:44:44 +0530

    Delete RAG SYSTEM/app.py

--- RAG SYSTEM/app.py
+++
@@ -1,8 +0,0 @@
-from retriever import retrieve
-from generator import generate_answer
-
-query = input("Ask something: ")
-context = retrieve(query)
-answer = generate_answer(context, query)
-
-print(answer)
